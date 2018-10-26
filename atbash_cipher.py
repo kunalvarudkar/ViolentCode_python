663b5d9676021ec18410d61aba1192b2fd7d888d
Kunal Sunil Varudkar <varudkar.kunal@gmail.com>
2018-10-26 21:35:41 +0530

    Add files via upload
    The code is on atbash cipher-> encodes 
    A=Z, B=Y, C=X and so on

---
+++ atbash_cipher.py
@@ -0,0 +1,36 @@
+#! /usr/bin/python
+# This script uses dictionaries to lookup various alphabets 
+lookup_table = {'A' : 'Z', 'B' : 'Y', 'C' : 'X', 'D' : 'W', 'E' : 'V', 
+		'F' : 'U', 'G' : 'T', 'H' : 'S', 'I' : 'R', 'J' : 'Q', 
+		'K' : 'P', 'L' : 'O', 'M' : 'N', 'N' : 'M', 'O' : 'L', 
+		'P' : 'K', 'Q' : 'J', 'R' : 'I', 'S' : 'H', 'T' : 'G', 
+		'U' : 'F', 'V' : 'E', 'W' : 'D', 'X' : 'C', 'Y' : 'B', 'Z' : 'A'} 
+
+def atbash(message): 
+	cipher = '' 
+	for letter in message: 
+		# checks for space 
+		if(letter != ' '): 
+			#adds the corresponding letter from the lookup_table 
+			cipher += lookup_table[letter] 
+		else: 
+			# adds space 
+			cipher += ' '
+
+	return cipher 
+
+# Driver function to run the program 
+def main(): 
+	#encrypt the given message 
+	message = raw_input("enter the message to encode=>")
+	print(atbash(message.upper())) 
+	
+	#decrypt the given message 
+	message = raw_input("enter the message to decode=>")
+	print(atbash(message.upper())) 
+
+
+# Executes the main function 
+if __name__ == '__main__': 
+	main() 
+
